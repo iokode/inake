61dfbe4a62e1af5ec62a5084a4fcd2893525527f
Ivan Montilla <montyclt@outlook.com>
2021-08-29 01:39:18 +0200

    Added a tryout program project

---
+++ IOKode.Inake.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using IOKode.Inake;
+using IOKode.Inake.RandomGenerators;
+
+var wordGenerator = new WordGenerator(new SystemRandomGenerator(new Random()));
+
+for (int i = 0; i < 10; i++)
+{
+    var word = wordGenerator.GenerateWord(8);
+    Console.WriteLine(word);
+}
+
